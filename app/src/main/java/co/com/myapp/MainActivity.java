674ba32a56e9cd591bf08b558ed2721c856a0268
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-09-04 07:51:20 -0500

    se crea ux/ui de la aplicacion de libros de movil 2

---
+++ app/src/main/java/co/com/myapp/MainActivity.java
@@ -0,0 +1,26 @@
+package co.com.myapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+            //hay tres maneras de inyectar dependencias, por metodos, o por los setters, la segunda manera es por el constructor, y la tercera manera es por la variable.
+
+        });
+    }
+}
